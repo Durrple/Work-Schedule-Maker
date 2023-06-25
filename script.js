e06db87dfded82c71ceb4a8fc16c1777c3acf6f0
[purrple durrple] <durrple@tuta.io>
2023-06-25 10:42:30 -0500

    finished js code for assigning color classes, finished js code for saving text data to local storage, etc.

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ function setTimeClass(hour, className) {
   getLocal(hour);
 }
 
+function timeIntervals() {
+  const timeSlots = 9;
+  for (let i = 9; i < timeSlots + 9; i++) {
+    console.log(`currentHour: ${currentHour}\nHour Checked: ${i}`);
+    if (currentHour < i) {
+      setTimeClass(i, 'future');
+    } else if (currentHour == i) {
+      setTimeClass(i, 'present');
+    } else {
+      setTimeClass(i, 'past');
+    }
+  }
+}
+
+function saveLocal(event) {
+  event.preventDefault();
+  const idHour = this.parentElement.id.split('-')[1];
+  const inputStr = `#input-${idHour}`;
+  const hourInput = $(inputStr).val();
+  console.log(idHour, hourInput);
+  localStorage.setItem(idHour, hourInput);
+}
+
+function getLocal(hour) {
+  const hourValue = localStorage.getItem(hour);
+  const inputStr = `#input-${hour}`;
+  $(inputStr).append(hourValue);
+}
+
+timeIntervals();
+$(":button").on("click", saveLocal);
